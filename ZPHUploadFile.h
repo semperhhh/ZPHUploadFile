9d55f3550df346d23cb1f37782bbd5f44a502b7a
zphui5409 <zphui5409@sina.com>
2018-01-31 15:18:16 +0800

    上传

---
+++ ZPHUploadFile.h
@@ -0,0 +1,19 @@
+//
+//  ZPHUploadFile.h
+//  ZPHDownImage
+//
+//  Created by zph on 31/01/2018.
+//  Copyright © 2018 zph. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSInteger,ZPHFileType){
+    ZPHFileTypeImage = 0,//图片
+    ZPHFileTypeVoice,   //音频
+};
+
+@interface ZPHUploadFile : NSObject
+
++(void)uploadRequestWithUrl:(NSString *)urlStr Data:(NSData *)fileData fileType:(ZPHFileType)fileType fileName:(NSString *)fileName success:(void(^)(NSData *successData))successBlock error:(void(^)(NSError *error))errorBlock;
+@end
